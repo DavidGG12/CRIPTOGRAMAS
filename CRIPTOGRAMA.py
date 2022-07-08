ff7f85bebcd39b15444d38d1aa82feac795e0155
Ian García <87337238+DavidGG12@users.noreply.github.com>
2022-07-08 11:58:37 -0500

    Puede aceptar signos de puntuación.

--- CRIPTOGRAMA.py
+++ CRIPTOGRAMA.py
@@ -23,36 +23,42 @@ numero = 0
 
 if(bandera == True):
     for x in palabra_cod:
-        if(x == ' '):
-            caracter = '#'
-            cod.append(caracter)
-
-        #Colocar una condición para poder aceptar signos de puntuación
-        else:
-            agregar = ord(x)-64
-            if(agregar <= 0):
-                x = int(x)
-                numero = x - (x * 2)
-                cod.append(numero)
+        try:
+            if(x == ' '):
+                caracter = '#'
+                cod.append(caracter)
             else:
-                cod.append(agregar)
-
+                agregar = ord(x)-64
+                if(agregar <= 0):
+                    x = int(x)
+                    numero = x - (x * 2)
+                    cod.append(numero)
+                elif(agregar > 26):
+                    cod.append(str(x))
+                else:
+                    cod.append(agregar)
+        except:
+            cod.append(str(x))
     print(cod)
 
     for i in cod:
-        if(i == '#'):
-            cod_bien.append(i)
-        else:
-            i = int(i)
-            num = i + 2
-            if(i <= 0):
+        try:
+            if(i == '#'):
                 cod_bien.append(i)
             else:
-                if(num == 27 or num == 28):
-                    num = num - 26  
-                    cod_bien.append(num)
+                i = int(i)
+                num = i + 2
+                if(i <= 0):
+                    cod_bien.append(i)
                 else:
-                    cod_bien.append(num)
+                    if(num == 27 or num == 28):
+                        num = num - 26  
+                        cod_bien.append(num)
+                    else:
+                        cod_bien.append(num)
+        except:
+            cod_bien.append(str(i))
+
     print(cod_bien)
     cod_transformado = convert(cod_bien)
     palabra_final = ""
